9c0cfc058c29400a019560c1ab72197212a4840a
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2021-10-23 09:45:25 -0700

    fixedButton

--- ShowcaseAppTemplate/app/src/main/java/com/mua/showcaseapptemplate/MainActivity.java
+++ ShowcaseAppTemplate/app/src/main/java/com/mua/showcaseapptemplate/MainActivity.java
@@ -3,14 +3,26 @@ package com.mua.showcaseapptemplate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loginBtn = findViewById(R.id.loginButton);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("LoggedIn with name");
+            }
+        });
 
     }
 }
