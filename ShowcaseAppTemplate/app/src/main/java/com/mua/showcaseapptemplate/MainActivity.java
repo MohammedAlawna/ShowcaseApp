31f0cbcd52168997d235cc546bff2fb28567cd5d
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2021-10-23 13:23:44 -0700

    mainActivitySupportForDifferentDensities.

--- ShowcaseAppTemplate/app/src/main/java/com/mua/showcaseapptemplate/MainActivity.java
+++ ShowcaseAppTemplate/app/src/main/java/com/mua/showcaseapptemplate/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button loginBtn;
+    EditText loginInputTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         loginBtn = findViewById(R.id.loginButton);
+        loginInputTxt = findViewById(R.id.name);
+
+
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
